0e76529b2f9911f4c7c59aa848d6e24ef436b33b
KristianWede <kris79s4@stud.kea.dk>
2022-03-11 14:10:06 +0100

    Added Uwuinator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+    public void Uwuing(){
+        System.out.println("UwU, mr shrek");
+    }
+
     public static void main(String[] args) {
+        Main obj = new Main();
+
+        obj.Uwuing();
+
         System.out.println("Hejsa og velkommen til troll cave1!!!1");
     }
 }
