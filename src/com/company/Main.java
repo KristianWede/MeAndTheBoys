edb9017ee8c1b6d9f9fb36634ab4daaecfb74fba
KristianWede <kris79s4@stud.kea.dk>
2022-03-11 14:16:38 +0100

    Added deportation of the danish method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    public void denDanskeMetode(){
+        System.out.println("klik her for at tjene 100 tusindvis af dollars");
+    }
+
     public void Ratata(){
         System.out.println("An' da ting goes ....");
     }
@@ -13,13 +17,10 @@ public class Main {
     public static void main(String[] args) {
         Main obj = new Main();
 
-
         obj.Uwuing();
         obj.denDanskeMetode();
 
         System.out.println("Hejsa og velkommen til troll cave1!!!1");
     }
-    public void denDanskeMetode(){
-        System.out.println("klik her for at tjene 100 tusindvis af dollars");
-    }
+
 }
