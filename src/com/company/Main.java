6904f602ee44660b3f2dbedf53f33ba08d5a7232
Ramilade <rami0904@stud.kea.dk>
2022-03-11 14:12:34 +0100

    Ratata metode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    public void Ratata(){
+        System.out.println("An' da ting goes ....");
+    }
+
     public void Uwuing(){
         System.out.println("UwU, mr shrek");
     }
@@ -10,6 +14,7 @@ public class Main {
         Main obj = new Main();
 
         obj.Uwuing();
+        obj.Ratata();
 
         System.out.println("Hejsa og velkommen til troll cave1!!!1");
     }
